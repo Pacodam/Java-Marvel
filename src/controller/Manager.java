ceeaae569997e1b39da1c1dcf845f3adbcf514e3
Paco <pacodam2@gmail.com>
2018-12-25 11:24:28 +0100

    option v (view superheros) 1

--- src/controller/Manager.java
+++ src/controller/Manager.java
@@ -5,11 +5,29 @@
  */
 package controller;
 
+import java.sql.SQLException;
+import java.util.List;
+import model.Superhero;
+import persistence.MarvelDAO;
+
 /**
  *
  * @author alu2017454
  */
 public class Manager {
     
-  
+    private static MarvelDAO marvelDAO;
+    
+    public Manager() {
+        marvelDAO = new MarvelDAO();
+    }
+    
+    
+  public List<Superhero> getSuperheroes() throws SQLException {
+      marvelDAO.connect();
+      List<Superhero> allHeroes = marvelDAO.selectAllHeroes();
+      marvelDAO.disconnect();
+      return allHeroes; 
+  }
+          
 }
